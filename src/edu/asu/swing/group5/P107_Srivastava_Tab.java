ed59e1e3e92142e53569e13e80879f19f1bce290
Varun <varun1309@bitbucket.org>
2018-01-20 01:40:59 -0700

    As per the instructions, adding 25 sample panels

---
+++ src/edu/asu/swing/group5/P107_Srivastava_Tab.java
@@ -0,0 +1,22 @@
+package edu.asu.swing.group5;
+
+import javax.swing.JTabbedPane;
+
+public class P107_Srivastava_Tab extends JTabbedPane{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5L;
+
+	public P107_Srivastava_Tab() {
+		add("Panel1", new Panel_1());
+		add("Panel1", new Panel_2());
+		add("Panel1", new Panel_3());
+		add("Panel1", new Panel_4());
+		add("Panel1", new Panel_5());
+	}
+}
+
+
+
--- src/edu/asu/swing/main/MainApplication.java
+++ src/edu/asu/swing/main/MainApplication.java
@@ -5,11 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
-import edu.asu.swing.group1.Tab_1;
-import edu.asu.swing.group2.Tab_2;
-import edu.asu.swing.group3.Tab_3;
-import edu.asu.swing.group4.Tab_4;
-import edu.asu.swing.group5.Tab_5;
+import edu.asu.swing.group5.P107_Srivastava_Tab;
 
 public class MainApplication extends JFrame {
 
@@ -33,11 +29,7 @@ public class MainApplication extends JFrame {
 					JTabbedPane jTabbedPane = new JTabbedPane();
 					
 					
-					jTabbedPane.addTab("Tab 1", new Tab_1());
-					jTabbedPane.addTab("Tab 2", new Tab_2());
-					jTabbedPane.addTab("Tab 3", new Tab_3());
-					jTabbedPane.addTab("Tab 4", new Tab_4());
-					jTabbedPane.addTab("Tab 5", new Tab_5());
+					jTabbedPane.addTab("Tab 5", new P107_Srivastava_Tab());
 					
 					
 					frame.add(jTabbedPane);
